86c44e04e0c48f6c5147ee8d140a1de15a828bf9
Sebastian Aldi <sebastianaldi@Sebastians-MacBook-Pro.local>
2024-08-21 19:38:57 +0700

    Move pattern to DB Changes: - Create new edge function to get pattern list - Move assets to firebase - Make frontend hit edge function to show patterns and examples - Renamed supabase-frontend to frontend, since the difference is just the base API

---
+++ frontend/src/pages/Patterns.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-switch v-model="loadVideo" label="Load video?" color="indigo"></v-switch>
+        </v-row>
+        <!-- <v-row v-if="!loadVideo">
+            <v-col cols="12" sm="6" md="4" v-for="pattern in patterns">
+                <v-row class="d-flex align-center justify-center">
+                    <v-img :src="pattern.thumbnail_url" max-width="320"></v-img>
+                </v-row>
+                <a class="d-flex align-center justify-center mt-4" :href="pattern.youtube_url">{{ pattern.name }}</a>
+            </v-col>
+        </v-row> -->
+        <v-row>
+            <v-col cols="12" sm="6" md="4" v-for="pattern in patterns">
+                <v-row class="d-flex align-center justify-center">
+                    <video width="320" controls preload="none" :poster="pattern.thumbnail_url"
+                        onloadstart="this.volume=0.5">
+                        <source :src="pattern.preview_url" v-if="loadVideo">
+                    </video>
+                </v-row>
+                <a class="d-flex align-center justify-center mt-4" :href="pattern.youtube_url">{{ pattern.name }}</a>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            loadVideo: false,
+            patterns: []
+        }
+    },
+    mounted() {
+        axios.get(`${import.meta.env.VITE_API_URL}/get-patterns`)
+        .then((resp) => {
+            for(let i = 0; i < resp.data.patterns.length; i += 1) {
+                let pattern = resp.data.patterns[i]
+                this.patterns.push(pattern)
+            }
+        })
+        .catch((err) => {
+            console.error(err)
+            alert("An error occured, please refresh the page")
+        })
+    }
+}
+</script>
---
+++ supabase-serverless/supabase/functions/get-patterns/index.ts
@@ -0,0 +1,67 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+// Setup type definitions for built-in Supabase Runtime APIs
+import "jsr:@supabase/functions-js/edge-runtime.d.ts"
+
+import * as postgres from 'https://deno.land/x/postgres@v0.17.0/mod.ts'
+
+const databaseUrl = Deno.env.get('SUPABASE_DB_URL')!
+const pool = new postgres.Pool(databaseUrl, 3, true)
+
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+}
+
+Deno.serve(async (req) => {
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders })
+  }
+
+  try {
+    const connection = await pool.connect()
+
+    try {
+      const result = await connection.queryObject`
+        SELECT
+          id, name, thumbnail_url, preview_url, youtube_url
+        FROM
+          patterns
+        ORDER BY
+          id
+      `
+
+      const patterns = { patterns: result.rows }
+
+      const body = JSON.stringify(patterns)
+
+      return new Response(body, {
+        status: 200,
+        headers: {
+          ...corsHeaders,
+          'Content-Type': 'application/json'
+        },
+      })
+
+    } finally {
+      connection.release()
+    }
+  } catch (err) {
+    console.error(err)
+    return new Response(String(err?.message ?? err), { status: 500 })
+  }
+})
+
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/get-patterns' \
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+    --header 'Content-Type: application/json' \
+    --data '{"name":"Functions"}'
+
+*/
